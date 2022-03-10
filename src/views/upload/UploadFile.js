4445bcd16559684950460c152b7cfabb82029bf5
Charlotte <charlottechuazn@gmail.com>
2022-03-11 02:45:23 +0800

    made frontend changes

--- src/views/upload/UploadFile.js
+++ src/views/upload/UploadFile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 
 export default function UploadFile() {
@@ -11,28 +10,28 @@ export default function UploadFile() {
               <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
                 <div className="flex-auto px-4 lg:px-10 py-10 pt-3">
                   <h6 className="text-blueGray-500 my-auto text-center font-bold">Upload CSV</h6>
-                  <div class="flex justify-center mt-8">
-                    <div class="rounded-lg shadow-xl bg-gray-50">
-                        <div class="m-4">
-                            <label class="inline-block mb-2 text-gray-500">File Upload</label>
-                            <div class="flex items-center justify-center">
+                  <div className="flex justify-center mt-8">
+                    <div className="rounded-lg shadow-xl bg-gray-50">
+                        <div className="m-4">
+                            <label className="inline-block mb-2 text-gray-500">File Upload</label>
+                            <div className="flex items-center justify-center">
                                 <label
-                                    class="flex flex-col w-full">
-                                    <div class="flex flex-col items-center justify-center pt-7">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8 text-gray-400 group-hover:text-gray-600"
+                                    className="flex flex-col w-full">
+                                    <div className="flex flex-col items-center justify-center pt-7">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-gray-400 group-hover:text-gray-600"
                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                 d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                         </svg>
-                                        <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                                        <p className="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
                                             Attach a file</p>
                                     </div>
-                                    <input type="file" class="opacity-0" />
+                                    <input type="file" className="opacity-0" />
                                 </label>
                             </div>
                         </div>
-                        <div class="flex justify-center p-2">
-                            <button class="w-full px-4 py-2 text-white bg-sky-500/100 rounded shadow-xl">Upload</button>
+                        <div className="flex justify-center p-2">
+                            <button className="w-full px-4 py-2 text-white bg-sky-500/100 rounded shadow-xl">Upload</button>
                         </div>
                     </div>
                 </div> 
