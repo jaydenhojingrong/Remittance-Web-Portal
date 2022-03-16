8199275d8dcf3e105dc82d52b98bc81f5bea8d7a
Tammy Lim <tamtam.limlim@gmail.com>
2022-03-16 17:32:10 +0800

    clear some FE files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
@@ -12,8 +12,6 @@ import Auth from "layouts/Auth.js";
 import Upload from "layouts/Upload.js"
 // views without layouts
 
-import Landing from "views/Landing.js";
-import Profile from "views/Profile.js";
 import Index from "views/Index.js";
 import MapFields from "views/MapFields.js"
 
@@ -26,10 +24,9 @@ ReactDOM.render(
       <Route path="/upload" component={Upload}/>
 
       {/* add routes without layouts */}
-      <Route path="/landing" exact component={Landing} />
-      <Route path="/profile" exact component={Profile} />
       <Route path="/mapfields" exact component={MapFields} />
       <Route path="/" exact component={Index} />
+      <Redirect from="/" to="/auth/login" />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -11,7 +11,6 @@ import FooterAdmin from "components/Footers/FooterAdmin.js";
 // views
 
 import Dashboard from "views/admin/Dashboard.js";
-import Maps from "views/admin/Maps.js";
 import Settings from "views/admin/Settings.js";
 import Tables from "views/admin/Tables.js";
 import Mapping from "views/admin/Mapping.js";
@@ -27,7 +26,6 @@ export default function Admin() {
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />
-            <Route path="/admin/maps" exact component={Maps} />
             <Route path="/admin/settings" exact component={Settings} />
             <Route path="/admin/tables" exact component={Tables} />
             <Route path="/admin/mapping" exact component={Mapping} />
