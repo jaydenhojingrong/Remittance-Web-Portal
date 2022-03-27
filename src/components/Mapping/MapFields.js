e74af5840df53a993c6e5265fd98147af0c67652
Tammy Lim <tamtam.limlim@gmail.com>
2022-03-28 00:51:55 +0800

    auto map done

--- src/components/Mapping/MapFields.js
+++ src/components/Mapping/MapFields.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Xwrapper } from "react-xarrows";
 import '../../assets/styles/Playground.css';
 import Box from '../Playground/Box';
@@ -10,139 +10,47 @@ import axios from "axios";
 
 function MapFields() {
   const [field, setField] = useState("");
-  const getFields = () => {
+  const [allHeaders, setAllHeaders] = useState([]);
+
+  useEffect(() => {
+    var config = {
+      headers: { 'Access-Control-Allow-Origin': '*' }
+    };
     axios
       .get(
-        "localhost:8080/headers/" + field
+        "http://localhost:8080/headers", config
       )
       .then((response) => {
         console.log(response);
-        console.log(response.data.ssotHeader);
+        // console.log(response.data);
+        setAllHeaders(response.data);
+        bindToInterface();
       })
       .catch((error) => {
         console.log(error);
       });
-  };
-  const [interfaces, setInterfaces] = useState([
-    {
-      id: 'first name',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'last name',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'address',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'country',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'static6',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'static123',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'a',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'c',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 's',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'd',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'w',
-      shape: 'interfaceBox',
-      type: 'input',
-    },
-    {
-      id: 'residence',
-      shape: 'interfaceBox',
-      type: 'output',
-    },
-    {
-      id: 'f name',
-      shape: 'interfaceBox',
-      type: 'output',
-    },
-  ]);
+  }, []);
 
-  const [inputHeaders, setInputHeaders] = useState([
-    {
-      id: 'first name',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'last name',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'address',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'country',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'static6',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'static123456',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'a',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'c',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 's',
-      shape: 'interfaceBox',
-    },
-  ]);
+  const [interfaces, setInterfaces] = useState([]);
 
-  const [ouputHeaders, setOuputHeaders] = useState([
-    {
-      id: 'residence',
-      shape: 'interfaceBox',
-    },
-    {
-      id: 'f name',
-      shape: 'interfaceBox',
-    },
-  ]);
-
+  const [inputHeaders, setInputHeaders] = useState([]);
+  const [ouputHeaders, setOuputHeaders] = useState([]);
   const [boxes, setBoxes] = useState([]);
   const [lines, setLines] = useState([]);
+
+  function bindToInterface() {
+    for (let i = 0; i < allHeaders.length; i++) {
+      console.log(allHeaders[i])
+      setInputHeaders(inputHeaders => [...inputHeaders, { id: allHeaders[i].currentHeader, shape: 'interfaceBox' }]);
+      setOuputHeaders(ouputHeaders => [...ouputHeaders, { id: allHeaders[i].ssotHeader, shape: 'interfaceBox' }]);
+      setLines(lines => [...lines, { props: { start: allHeaders[i].currentHeader, end: allHeaders[i].ssotHeader } }]);
+    }
+  }
+
+  function checkstate() {
+    console.log(lines);
+    console.log('test');
+  }
 
   // selected:{id:string,type:"arrow"|"box"}
   const [selected, setSelected] = useState(null);
@@ -201,11 +109,6 @@ function MapFields() {
                       .map((itr) => (
                         <Box {...boxProps} key={itr.id} id={itr.id} box={{ ...itr, id: itr.id }} position="static" sidePos="left" />
                       ))}
-                    {/* {interfaces
-                      .filter((itr) => itr.type === 'input')
-                      .map((itr) => (
-                        <Box {...boxProps} key={itr.id} box={{ ...itr, id: itr.id }} position="static" sidePos="left" />
-                      ))} */}
                   </div>
                   <div
                     id="boxesContainer"
@@ -229,18 +132,13 @@ function MapFields() {
                       .map((itr) => (
                         <Box {...boxProps} key={itr.id} id={itr.id} box={{ ...itr, id: itr.id }} position="static" sidePos="right" />
                       ))}
-                    {/* {interfaces
-                      .filter((itr) => itr.type === 'output')
-                      .map((itr) => (
-                        <Box {...boxProps} key={itr.id} box={{ ...itr, id: itr.id }} position="static" sidePos="right" />
-                      ))} */}
                   </div>
                   {/* xarrow connections*/}
                   {lines.map((line, i) => (
                     <Xarrow
                       key={line.props.root + '-' + line.props.end + i}
                       line={line}
-                      selected={selected} 
+                      selected={selected}
                       setSelected={setSelected}
                     />
                   ))}
@@ -257,6 +155,7 @@ function MapFields() {
           </div>
         </div>
       </div>
+      <button onClick={checkstate}> test </button>
     </>
   );
 }
--- src/main/java/com/OOP/remittancesystem/controller/HeaderController.java
+++ src/main/java/com/OOP/remittancesystem/controller/HeaderController.java
@@ -1,5 +1,6 @@
 package com.OOP.remittancesystem.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ public class HeaderController {
 	 * @return List of headers
 	 */
 	@GetMapping("/headers")
+	@CrossOrigin(origins = "http://localhost:3000")
 	public List <HeaderNames> getAllHeaders(HeaderNames headerNames) {
 		return headerService.getAllHeaders();
 	}
