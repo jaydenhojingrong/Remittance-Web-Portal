0358c6276ee30ab894fe0cbb9acce4e9d4b025d4
Shawntwj <shawntwj20@gmail.com>
2022-04-04 21:29:48 +0800

    validation connect to DAO

--- src/main/java/com/OOP/remittancesystem/service/ValidationService.java
+++ src/main/java/com/OOP/remittancesystem/service/ValidationService.java
@@ -4,41 +4,50 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.regex.*;  
+import java.util.regex.*;
 
+import com.OOP.remittancesystem.entity.HeaderNames;
+import com.OOP.remittancesystem.service.HeaderService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ValidationService {
 
+	@Autowired
+	private HeaderService headerService;
+	
+
     private boolean spoil = false;
     private ArrayList<String> whatSpoilList = new ArrayList<String>();
 
     public String getSsotHeader(String company,String columnName){
-        // get SSOT header
-        String ssotHeader = "sFirstName";
-        return columnName;
+        
+        HeaderNames ssotHeaderEntity = headerService.getSsotByCurrentHeaderAndCompany(columnName,  company);
+        String ssotHeader = ssotHeaderEntity.getSsotHeader();
+        return ssotHeader;
     }
 
-    public String getSize(String ssotHeader){
-        // get size validation
-        return "0|20";
+    public String getSize(String ssotHeader, String company){
+        String sizeReq = headerService.getSizeByApiHeaderAndCompany(ssotHeader,  company);
+        return sizeReq;
+        // return "0|20";
     }
 
-    public String getValidation(String ssotHeader ){
-        // get regex
-        return "^[A-Za-z0-9 .-]+$";
+    public String getValidation(String ssotHeader, String company ){
+        String sizeReq = headerService.getSizeByApiHeaderAndCompany(ssotHeader,  company);
+        return sizeReq;
     }
 
-    public boolean sizeValidation(String value, String ssotHeader){
+    public boolean sizeValidation(String value, String ssotHeader, String company){
 
-        String size = getSize(ssotHeader);
+        String size = getSize(ssotHeader, company);
 
         if (size == null){
             return true;
         }
 
-        Scanner s = new Scanner(size);
         String[] sizeArray = size.split("\\|");
 
         int min = Integer.parseInt(sizeArray[0]);
@@ -57,9 +66,9 @@ public class ValidationService {
         }
     }
     
-    public boolean regexValidation(String value, String ssotHeader)  {
+    public boolean regexValidation(String value, String ssotHeader, String company)  {
 
-        String validation = getValidation(ssotHeader);
+        String validation = getValidation(ssotHeader, company);
         
         if (validation == null){
             return true;
