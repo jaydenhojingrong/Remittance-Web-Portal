299383b84a962be3f244fcd23fb68ac416379204
Charlotte <charlottechuazn@gmail.com>
2022-04-04 19:18:26 +0800

    update remittance service.java

--- src/main/java/com/OOP/remittancesystem/service/RemittanceService.java
+++ src/main/java/com/OOP/remittancesystem/service/RemittanceService.java
@@ -2,6 +2,8 @@ package com.OOP.remittancesystem.service;
 
 import com.OOP.remittancesystem.entity.EverywhereRemit;
 import com.OOP.remittancesystem.entity.Remittance;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.OOP.remittancesystem.dao.RemittanceDAO;
 
 import org.springframework.stereotype.Service;
@@ -10,6 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Map;
+
+import org.json.*;
+
 
 @Service
 public class RemittanceService {
@@ -22,7 +27,7 @@ public class RemittanceService {
     }
 
     //     @GetMapping("/convert")
-    public String toJSON(Map<String,List<String>> map) {
+    public JSONObject toJSON(Map<String,List<String>> map) throws JSONException{
         String body = "{";
         int i = 0;
         for(Map.Entry<String,List<String>> e : map.entrySet()) {
@@ -44,9 +49,13 @@ public class RemittanceService {
             i++;
         }
         body += "}";
-        return body;
+        System.out.println("--------------------------------------------------------------------------");
+        JSONObject jsonObject = new JSONObject(body);
+
+        return jsonObject;
+        }
     }
-}
+
 
 
 
