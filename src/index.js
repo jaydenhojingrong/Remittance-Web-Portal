afa804d98de291f82e97d6bbe9ffce85c190681a
Tammy Lim <tamtam.limlim@gmail.com>
2022-04-05 05:06:54 +0800

    clear fe files

--- src/index.js
+++ src/index.js
@@ -14,7 +14,6 @@ import Upload from "layouts/Upload.js"
 // views without layouts
 
 import Index from "views/Index.js";
-import MapFields from "views/MapFields.js"
 
 ReactDOM.render(
   <BrowserRouter>
@@ -25,7 +24,6 @@ ReactDOM.render(
       <Route path="/upload" component={Upload}/>
 
       {/* add routes without layouts */}
-      <Route path="/mapfields" exact component={MapFields} />
       <Route path="/" exact component={Index} />
       <Redirect from="/" to="/auth/login" />
     </Switch>
